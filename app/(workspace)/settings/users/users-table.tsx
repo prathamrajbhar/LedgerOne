d38b19c1a5ec7220a9f6fada9e554e74584376da
prathamrajbhar <pratham.rajbhar987@gmail.com>
2026-09-19 08:37:22 +0530

    fix(build): remove unused Link import and stray e2e scripts to resolve Vercel deployment error

--- app/(workspace)/settings/users/users-table.tsx
+++ app/(workspace)/settings/users/users-table.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import Link from "next/link";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
